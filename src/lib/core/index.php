d65a7d9f7b26c864f27f8d90ec4d1d85082587f2
ogius <kowalewski.olgierd@gmail.com>
2024-07-04 00:54:14 +0200

    started working on core library

---
+++ src/lib/core/index.php
@@ -0,0 +1,8 @@
+<?php namespace core;
+require_once "http/index.php";
+// require_once "res/index.php";
+// require_once "req/index.php";
+
+// Request::set_routes_dir(__DIR__."/routes");
+// $response = Request::load()->send();
+// $response->send();
