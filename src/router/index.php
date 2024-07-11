bf6c677c3f54db1420642fcbed0921b04618389f
ogius <kowalewski.olgierd@gmail.com>
2024-07-11 13:54:13 +0200

    created router file structure

---
+++ src/router/index.php
@@ -0,0 +1,4 @@
+<?php namespace fusion\router;
+require_once __DIR__."/traits/index.php";
+require_once __DIR__."/value_objects/index.php";
+require_once __DIR__."/router/index.php";
---
+++ src/router/router/index.php
@@ -0,0 +1,3 @@
+<?php namespace fusion\router;
+// require_once __DIR__."/interface.php";
+use fusion\http\{Req, Res};
