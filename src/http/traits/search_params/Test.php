1c63788744d27a63480c1c7bf80254cf323c10b4
ogius <kowalewski.olgierd@gmail.com>
2024-07-11 03:43:24 +0200

    updated search params test

--- src/http/traits/search_params/Test.php
+++ src/http/traits/search_params/Test.php
@@ -1,80 +1,47 @@
 <?php namespace fusion\http\traits;
 
-class SearchParamsTest extends \PHPUnit\Framework\TestCase{
-  function test_interface_matching() {
-    new class implements ISearchParams {
-      use SearchParams;
-    };
-    $this->assertTrue(true);
-  }
+class SearchParamsTested implements ISearchParams {
+  use SearchParams;
 
+  function __construct(string $url){
+    $this->initSearchParams($url);
+  }
+}
+
+class SearchParamsTest extends \PHPUnit\Framework\TestCase{
+  
   function test_init_with_empty_string_as_valid_input() {
-    new class implements ISearchParams {
-      use SearchParams;
-      function __construct(){
-        $this->initSearchParams("");
-      }
-    };
+    new SearchParamsTested("");
     $this->assertTrue(true);
   }
 
   function test_init_with_valid_input() {
-    new class implements ISearchParams {
-      use SearchParams;
-      function __construct(){
-        $this->initSearchParams("a=1&b=2");
-      }
-    };
+    new SearchParamsTested("a=1&b=2");
     $this->assertTrue(true);
   }
 
   function test_init_with_slash_as_invalid_input() {
     $this->expectException(\InvalidArgumentException::class);
-    new class implements ISearchParams {
-      use SearchParams;
-      function __construct(){
-        $this->initSearchParams("/");
-      }
-    };
+    new SearchParamsTested("/");
   }
 
   function test_init_with_question_mark_as_invalid_input() {
     $this->expectException(\InvalidArgumentException::class);
-    new class implements ISearchParams {
-      use SearchParams;
-      function __construct(){
-        $this->initSearchParams("?");
-      }
-    };
+    new SearchParamsTested("?");
   }
 
   function test_get_search_params() {
-    $instance = new class implements ISearchParams {
-      use SearchParams;
-      function __construct(){
-        $this->initSearchParams("a=1&b=2");
-      }
-    };
+    $instance = new SearchParamsTested("a=1&b=2");
     $this->assertEquals($instance->get_search_params(), ["a"=>1, "b"=>2]);
   }
 
   function test_get_search_param_with_existing_param() {
-    $instance = new class implements ISearchParams {
-      use SearchParams;
-      function __construct(){
-        $this->initSearchParams("a=1&b=2");
-      }
-    };
+    $instance = new SearchParamsTested("a=1&b=2");
     $this->assertEquals($instance->get_search_param("a"), "1");
   }
 
   function test_get_search_param_with_non_existing_param() {
-    $instance = new class implements ISearchParams {
-      use SearchParams;
-      function __construct(){
-        $this->initSearchParams("a=1&b=2");
-      }
-    };
+    $instance = new SearchParamsTested("a=1&b=2");
     $this->assertEquals($instance->get_search_param("c"), null);
   }
 }
